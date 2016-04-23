346361289e767eb7d1069039b272584cd674b052
Belinda Vennam <beemarie88@gmail.com>
2016-04-23 11:54:02 -0400

    Initial Commit

---
+++ FoodiePlace/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodiePlace
+//
+//  Created by Belinda Vennam on 4/23/16.
+//  Copyright © 2016 Belinda Vennam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
