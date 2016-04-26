9afa79a3b9ae57866066d9cb0a778d52b22215ed
Belinda Vennam <beemarie88@gmail.com>
2016-04-26 10:55:41 -0400

    adding pod file for google maps as well as search bar boilerplate code for googlemaps.

--- FoodiePlace/LocationViewController.swift
+++ FoodiePlace/LocationViewController.swift
@@ -7,13 +7,34 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class LocationViewController: UIViewController {
+    var resultsViewController: GMSAutocompleteResultsViewController?
+    var searchController: UISearchController?
+    var resultView: UITextView?
+    //@IBOutlet weak var typeView: UIView!
     
     @IBOutlet weak var locationText: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        resultsViewController = GMSAutocompleteResultsViewController()
+        resultsViewController?.delegate = self
+        
+        searchController = UISearchController(searchResultsController: resultsViewController)
+        searchController?.searchResultsUpdater = resultsViewController
+        
+        let subView = UIView(frame: CGRectMake(0, 65.0, 400.0, 45.0))
+        
+        subView.addSubview((searchController?.searchBar)!)
+        self.view.addSubview(subView)
+        searchController?.searchBar.sizeToFit()
+        searchController?.hidesNavigationBarDuringPresentation = false
+        
+        // When UISearchController presents the results view, present it in
+        // this view controller, not one further up the chain.
+        self.definesPresentationContext = true
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -23,8 +44,34 @@ class LocationViewController: UIViewController {
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        let destinationVC = segue.destinationViewController as! FoodieTableViewController
-        destinationVC.location = locationText.text
+        //let destinationVC = segue.destinationViewController as! FoodieTableViewController
+        //destinationVC.location = locationText.text
+    }
+}
+// Handle the user's selection.
+extension LocationViewController: GMSAutocompleteResultsViewControllerDelegate {
+    func resultsController(resultsController: GMSAutocompleteResultsViewController,
+                           didAutocompleteWithPlace place: GMSPlace) {
+        searchController?.active = false
+        // Do something with the selected place.
+        print("Place name: ", place.name)
+        print("Place address: ", place.formattedAddress)
+        print("Place attributions: ", place.attributions)
+    }
+    
+    func resultsController(resultsController: GMSAutocompleteResultsViewController,
+                           didFailAutocompleteWithError error: NSError){
+        // TODO: handle the error.
+        print("Error: ", error.description)
+    }
+    
+    // Turn the network activity indicator on and off again.
+    func didRequestAutocompletePredictionsForResultsController(resultsController: GMSAutocompleteResultsViewController) {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+    }
+    
+    func didUpdateAutocompletePredictionsForResultsController(resultsController: GMSAutocompleteResultsViewController) {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = false
     }
 }
 
